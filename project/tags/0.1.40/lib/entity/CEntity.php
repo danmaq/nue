90704e2583322edc9efc8fc38e13715e3c7a6ed3
danmaq <info@danmaq.com>
2011-06-05 13:16:22 +0000

    Version 0.1.40
    記事の詳細表示/編集/削除機能を追加。
    ユーザの退会機能を追加。
    疑似HTML(リンクと改行のみ)/特殊文字/複数段落の投稿に対応。
    記事一覧において投稿日時表示に対応。
    各ページで個別に実装していたエラー表示機能を統一した。
    トップページはページ名を出さず、サイト名のみ出力するようにした。
    投稿やユーザ名などにおいて、先頭や末尾の空白文字を除去するようにした。
    
    63KBを超える記事を作成するとデータがクラッシュする不具合を修正。
    記事一覧表示画面において、ログオンしていない状態だと落ちる不具合を修正。
    記事投稿に不備があった場合、戻るべきでない場所に戻ってしまう不具合を修正。

---
+++ project/tags/0.1.40/lib/entity/CEntity.php
@@ -0,0 +1,125 @@
+<?php
+
+require_once(NUE_LIB_ROOT . '/state/CEmptyState.php');
+
+/**
+ *	状態を持ったオブジェクト。
+ */
+class CEntity
+{
+
+	/**	前回の状態。 */
+	private $previousState;
+
+	/**	現在の状態。 */
+	private $currentState;
+
+	/**	次の状態。 */
+	private $nextState;
+
+	/**
+	 *	コンストラクタ。
+	 *
+	 *	@param $firstState 最初の状態。既定ではnull。
+	 */
+	public function __construct(IState $firstState = null)
+	{
+		$this->previousState = CEmptyState::getInstance();
+		$this->currentState = CEmptyState::getInstance();
+		$this->setNextState($firstState);
+		$this->commitNextState();
+	}
+
+	/**
+	 *	デストラクタ。
+	 */
+	function __destruct()
+	{
+		$this->dispose();
+	}
+
+	/**
+	 *	前回の状態を取得します。
+	 *
+	 *	@return IState 前回の状態。
+	 */
+	public function getPreviousState()
+	{
+		return $this->previousState;
+	}
+
+	/**
+	 *	現在の状態を取得します。
+	 *
+	 *	@return IState 現在の状態。
+	 */
+	public function getCurrentState()
+	{
+		return $this->currentState;
+	}
+
+	/**
+	 *	次の状態を取得します。
+	 *
+	 *	@return IState 次の状態。
+	 */
+	public function getNextState()
+	{
+		return $this->nextState;
+	}
+
+	/**
+	 *	次の状態を設定します。
+	 *
+	 *	@patam IState $nextState 次の状態。
+	 */
+	public function setNextState(IState $nextState = null)
+	{
+		$this->nextState = $nextState;
+	}
+
+	/**
+	 *	実行します。
+	 */
+	public function execute()
+	{
+		$this->getCurrentState()->execute($this);
+		$this->commitNextState();
+	}
+
+	/**
+	 *	予約された次の状態を確定します。
+	 *
+	 *	@param boolean 状態が遷移した場合、true。
+	 */
+	public function commitNextState()
+	{
+		if($this->getNextState() !== null)
+		{
+			$this->getCurrentState()->teardown($this);
+		}
+		$result = $this->getNextState() !== null;
+		if($result)
+		{
+			$this->previousState = $this->currentState;
+			$this->currentState = $this->nextState;
+			$this->nextState = null;
+			$this->getCurrentState()->setup($this);
+		}
+		return $result;
+	}
+
+	/**
+	 *	状態をリセットします。
+	 */
+	public function dispose()
+	{
+		$this->setNextState(CEmptyState::getInstance());
+		$this->commitNextState();
+		$this->previousState = CEmptyState::getInstance();
+		$this->currentState = CEmptyState::getInstance();
+		$this->nextState = null;
+	}
+}
+
+?>
---
+++ project/tags/0.1.40/sql/tag/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO NUE_INDEX_TAG
+(
+	NAME,
+	ENTITY_ID
+)
+VALUES
+(
+	:id,
+	:entity_id
+);
