b0ac6a62c9f6a697c3e4e591c6acc90e0e2e084f
danmaq <info@danmaq.com>
2011-06-10 00:54:39 +0000

    Version 0.3.56
    一部DDLの主キー設定がデータ構造と矛盾する不具合を修正。
    タグの親子関係ツリー構造キャッシュ用SQLを作成。
    タグの親子関係ツリー構造キャッシュDAOをガワだけ作成。

--- project/trunk/lib/CConstants.php
+++ project/trunk/lib/CConstants.php
@@ -14,7 +14,7 @@ class CConstants
 {
 
 	/** バージョン情報。 */
-	const VERSION = '0.3.55';
+	const VERSION = '0.3.56';
 
 	/** セッション名。 */
 	const SESSION_CORE = 'NUE_Core';
---
+++ project/trunk/lib/file/CFileSQLTagTree.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once('CFileCache.php');
+
+/**
+ *	記事SQL用ファイル キャッシュ。
+ */
+class CFileSQLTagTree
+	extends CFileCache
+{
+
+	/**	クラス オブジェクト。 */
+	private static $instance = null;
+
+	/**
+	 *	クラス オブジェクトを取得します。
+	 *
+	 *	@return CFileCache クラス オブジェクト。
+	 */
+	public static function getInstance()
+	{
+		if(self::$instance == null)
+		{
+			self::$instance = new CFileSQLTagTree();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 *	コンストラクタ。
+	 */
+	protected function __construct()
+	{
+		parent::__construct(NUE_ROOT . '/sql/tag/tree');
+	}
+
+	/**
+	 *	不明なプロパティが呼ばれた際に呼び出されます。
+	 *
+	 *	ここでは、プロパティ名をファイルと見なし呼び出します。
+	 *
+	 *	@param ファイル名。
+	 *	@return ファイル内容文字列。
+	 */
+	public function __get($name)
+	{
+		return $this->load($name . '.sql');
+	}
+}
+
+?>
