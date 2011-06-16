b23554396721c4fb019aff7fd4feada78696f626
danmaq <danmaq@a1837275-2e75-43a8-8ef6-c1e9457507e2>
2011-06-16 03:28:22 +0000

    新XSL実装の続き。

--- project/trunk/skin/danmaq(sample)/default.js
+++ project/trunk/skin/danmaq(sample)/default.js
@@ -1,4 +1,5 @@
-// alert('ほげ');
+
+////////////////////////////////////////////////////////////
 
 /**
  *	記事ウィンドウ クラス。
@@ -7,19 +8,41 @@
  */
 function CSection(section)
 {
-	this.section = section;
-	return this;
+	this.section = $(section);
+	this.titleBar = this.section.find('h2.title');
+	this.toString = function()
+	{
+		return "[CSection]";
+	}
+	this.toggleVisible = function()
+	{
+		alert(this.titleBar);
+	}
+
+	/** Constructor */
+	{
+		var titleBar = this.titleBar;
+		titleBar.css('cursor', 'pointer');
+		titleBar.click(this.toggleVisible);
+		// TODO : h2に下記設定
+		// :hover着色
+		// onClick: toggleVisible()
+	}
 }
 
 var m_sections = new Array();
 
 window.onload = function()
 {
-	var sections = $('.section');
+	var sections = $('.onscript .section');
 	for(var i = sections.length; --i >= 0; )
 	{
-		this.m_sections.push(new CSection(item));
+		m_sections.push(new CSection(sections[i]));
 	}
-	// TODO : 
-	alert($('#body'));
+
+	// TODO : コメント外す
+	//$('#body').attr('style', 'display: relative;');
 };
+
+// TODO : 折り畳みメニュー
+// TODO : 画像の変換(XSLもまだ途中)
