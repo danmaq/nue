4638566c067d0eba41e679d1db646289537a7515
danmaq <danmaq@a1837275-2e75-43a8-8ef6-c1e9457507e2>
2011-06-19 15:31:57 +0000

    Version 0.4.69
    ファイル容量を取得する機能を追加。
    記事全件検索を行うとソート順が逆になる不具合を修正。
    日本語タグを設定すると化ける不具合を修正。
    記事書き込み時にゴミログが吐かれる不具合を修正。

---
+++ project/trunk/hlml/fsize.php
@@ -0,0 +1,25 @@
+<?php
+
+$fsize = -1;
+if(isset($attrs['path']))
+{
+	$fpath = NUE_ROOT . '/' . $attrs['path'];
+	if(file_exists($fpath))
+	{
+		$fsize = filesize($fpath);
+		if($fsize === false)
+		{
+			$fsize = -1;
+		}
+	}
+
+	// 0.00MB –hŽ~ô
+	if($fsize > 0 && $fsize < 10486)
+	{
+		$fsize = 10486;
+	}
+}
+
+$result = $dom->createTextNode(sprintf('%.2F KB', $fsize / 1048576));
+
+?>
